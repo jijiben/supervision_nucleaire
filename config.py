abbdbcbc2d15fd3dcdd8206156c5233f2edb411f
unknown <jihane.benkoulouch@gmail.com>
2023-06-12 16:30:20 +0200

    add api credential

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+CLIENT_ID = 'c2503e75-3492-4a26-8311-5378a1e51a78'
+SECRET_ID = 'a167f653-ffa8-4bb7-a61b-517d31e60417'
+
+
+TOKEN_URL = 'https://digital.iservices.rte-france.com/token/oauth/'
+ACTUAL_GENERATION_PER_UNIT_URL = 'https://digital.iservices.rte-france.com/open_api/actual_generation/v1/actual_generations_per_unit'
